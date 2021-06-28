3da41c23127fd16cffb90cab9ff977dcf5824d26
RobertBaratheonMoya <72909867+RobertBaratheonMoya@users.noreply.github.com>
2021-06-28 13:44:23 -0500

    Uploading a new app

---
+++ Shared/iOsMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOsMobileApp.swift
+//  Shared
+//
+//  Created by Roberto Moya on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct iOsMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
